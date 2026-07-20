5632d555fe7ed715b4c3ee0ac0fcfc2d297d2059
iqbalj <iqballabqi127@gmail.com>
2026-07-20 20:13:33 +0700

    chore(admin): update UserAdmin fieldsets for new user model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "first_name", "last_name", "is_staff"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "first_name", "last_name", "password1", "password2")}),
+    )
