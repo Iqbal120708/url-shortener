c715c4561d53bb1a9ee0eca2674f235eeac06b47
iqbalj <iqballabqi127@gmail.com>
2026-08-13 15:04:26 +0700

    fix(generate_data): handle duplicate dummy user and reduce seed rows to 200

--- src/short_url/management/commands/generate_data.py
+++ src/short_url/management/commands/generate_data.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 
 from short_url.models import ShortUrl
 from short_url.utils import generate_short_code
+from django.db import IntegrityError
 
 User = get_user_model()
 
@@ -11,12 +12,15 @@ class Command(BaseCommand):
     help = "Generate 1 000 000 rows short url"
 
     def handle(self, *args, **options):
-        user = User.objects.create_user(
-            first_name="first",
-            last_name="last",
-            email="dummydata@gmail.com",
-            password="secret1234",
-        )
+        try: 
+            user = User.objects.create_user(
+                first_name="first",
+                last_name="last",
+                email="dummydata@gmail.com",
+                password="secret1234",
+            )
+        except IntegrityError:
+            user = User.objects.get(email="dummydata@gmail.com")
 
         data = [
             ShortUrl(
@@ -24,8 +28,8 @@ class Command(BaseCommand):
                 original_url="https://dev.to/iqbal120708/preventing-overselling-with-stock-reservation-and-selectforupdate-in-django-3jam",
                 user=user,
             )
-            for _ in range(1_000_000)
+            for _ in range(200)
         ]
         ShortUrl.objects.bulk_create(data, batch_size=10000)
 
-        self.stdout.write("Successfully generte 1 000 000 rows data")
+        self.stdout.write("Successfully generte 200 rows data")
